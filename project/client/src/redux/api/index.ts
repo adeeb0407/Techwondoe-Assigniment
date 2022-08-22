031e587e9599cc68df4315d3630e0f50e2add435
adeeb0407 <adeebshah4@gmail.com>
2022-08-22 21:05:55 +0530

    dockerized the application

--- project/client/src/redux/api/index.ts
+++ project/client/src/redux/api/index.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
-export const API = axios.create({ baseURL: 'http://localhost:8001' });
+export const API = axios.create({ baseURL: 'http://localhost:8001/' });
+// export const API = axios.create({ baseURL: '/api/' });
 
 API.interceptors.request.use((req: any) => {
     if (cookies.get('user_info')) {
@@ -12,9 +13,9 @@ API.interceptors.request.use((req: any) => {
 });
 console.log(cookies.get('user_info')?.token)
 
-export const loginUser = (username: String, password: String) => API.post('/login', {username : username, password : password});
-export const createReview = (user_id: Number, title: String, rating:any, streamingApps : any, reviewDescription : String, banner :any) => API.post('/createreview',{user_id, title, rating, streamingApps, reviewDescription, banner});
-export const updateReview = (review_id: Number, title: String, rating:any, streamingApps : any, reviewDescription : String, banner :any) => API.patch('/updateReview', {review_id, title, rating, streamingApps, reviewDescription});
-export const myReviews = (user_id: Number) => API.get(`/reviews/${user_id}`);
-export const viewReview = (review_id: Number) => API.get(`/reviewsingle/${review_id}`);
-export const deleteReview = (review_id: Number) => API.delete(`/deletereview/${review_id}`);
+export const loginUser = (username: String, password: String) => API.post('login', {username : username, password : password});
+export const createReview = (user_id: Number, title: String, rating:any, streamingApps : any, reviewDescription : String, banner :any) => API.post('createreview',{user_id, title, rating, streamingApps, reviewDescription, banner});
+export const updateReview = (review_id: Number, title: String, rating:any, streamingApps : any, reviewDescription : String, banner :any) => API.patch('updateReview', {review_id, title, rating, streamingApps, reviewDescription});
+export const myReviews = (user_id: Number) => API.get(`reviews/${user_id}`);
+export const viewReview = (review_id: Number) => API.get(`reviewsingle/${review_id}`);
+export const deleteReview = (review_id: Number) => API.delete(`deletereview/${review_id}`);
--- project/server/src/config/config.ts
+++ project/server/src/config/config.ts
@@ -3,9 +3,10 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+
 export default mysql.createConnection({
     user: process.env.USER,
     host : process.env.HOST,
     password : process.env.PASSWORD,
-    database : process.env.DATABASE,
+    database : process.env.DATABASE
 })
